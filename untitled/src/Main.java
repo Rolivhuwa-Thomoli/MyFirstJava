2055f245a5452befaea39d1e19978daa9c5bc296
Rolivhuwa-Thomoli <rolivhuwathomoli@gmail.com>
2025-02-21 18:48:53 +0200

    fixing new functions

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,11 @@
 import java.util.Scanner;
+public class ClearConsole {
+    public static void clear() {
+        for (int i = 0; i < 50; ++i) {
+            System.out.println();
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
         String name;
@@ -7,5 +14,13 @@ public class Main {
         System.out.print("What is your name? : ");
         name = inputobj.nextLine();
         System.out.println("Thank you " + name+"! Nice to meet you.");
+        // Wait for 3 seconds (3000 milliseconds)
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        clear();
+        System.out.println("Now lets try adding 3 integers");
     }
 }
