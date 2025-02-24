f5766a80ac805ac326c109abb99b86f7f7872387
Rolivhuwa-Thomoli <rolivhuwathomoli@gmail.com>
2025-02-24 18:49:14 +0200

    Managed to fix the clear and time delay functions

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
-public class ClearConsole {
-    public static void clear() {
+
+public class Main {
+
+    //clear function
+     public static void clear() {
         for (int i = 0; i < 50; ++i) {
             System.out.println();
         }
     }
-}
-public class Main {
+    public static void timedelay(int millisec){
+        try {
+            Thread.sleep(millisec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         String name;
         Scanner inputobj = new Scanner(System.in);
@@ -15,12 +23,22 @@ public class Main {
         name = inputobj.nextLine();
         System.out.println("Thank you " + name+"! Nice to meet you.");
         // Wait for 3 seconds (3000 milliseconds)
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        timedelay(3000);
+        //
         clear();
-        System.out.println("Now lets try adding 3 integers");
+        System.out.println("Now lets try adding 3 integers OK!");
+        //Wait for 2 seconds (2000 millisecons)
+        timedelay(2000);
+        //
+        System.out.print("Please Enter num1: ");
+        int num1 = inputobj.nextInt();
+        System.out.print("Please Enter num2: ");
+        int num2 = inputobj.nextInt();
+        System.out.print("Please Enter num3: ");
+        int num3 = inputobj.nextInt();
+        System.out.println("your sum is : "+ (num1+num2+num3));
+        timedelay(4000);
+        clear();
+        System.out.println("Thank you");
     }
 }
